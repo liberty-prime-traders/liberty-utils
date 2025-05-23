c502e02954f50286b0d0d946009bc90c0291dee5
Ezra Orina <Ezra.Orina@nisc.coop>
2025-05-23 00:40:49 -0500

    add millisecond serialization

--- src/main/kotlin/me/ezrahome/libertyutils/dailysnapshot/business/user_location/UserLocationResponseDto.kt
+++ src/main/kotlin/me/ezrahome/libertyutils/dailysnapshot/business/user_location/UserLocationResponseDto.kt
@@ -1,6 +1,8 @@
 package me.ezrahome.libertyutils.dailysnapshot.business.user_location
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
 import me.ezrahome.libertyutils.dailysnapshot.model.LibertyLocation
+import me.ezrahome.libertyutils.platform.configuration.serializer.DatesToMillis
 import java.io.Serializable
 import java.time.OffsetDateTime
 import java.util.UUID
@@ -9,6 +11,10 @@ data class UserLocationResponseDto(
     var id: UUID? = null,
     var userName: String? = null,
     var location: LibertyLocation? = null,
+
+    @JsonSerialize(using = DatesToMillis::class)
     var startOn: OffsetDateTime? = null,
+
+    @JsonSerialize(using = DatesToMillis::class)
     var endOn: OffsetDateTime? = null
 ): Serializable
