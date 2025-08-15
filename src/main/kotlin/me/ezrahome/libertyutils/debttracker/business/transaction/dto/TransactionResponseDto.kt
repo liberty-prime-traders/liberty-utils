7841a34858b7656da081e45cb0d6cc98f1feba1a
Ezra Orina <Ezra.Orina@nisc.coop>
2025-08-14 21:12:42 -0500

    return string instead of LocalDate

--- src/main/kotlin/me/ezrahome/libertyutils/debttracker/business/transaction/dto/TransactionResponseDto.kt
+++ src/main/kotlin/me/ezrahome/libertyutils/debttracker/business/transaction/dto/TransactionResponseDto.kt
@@ -3,7 +3,6 @@ package me.ezrahome.libertyutils.debttracker.business.transaction.dto
 import me.ezrahome.libertyutils.dailysnapshot.model.LibertyLocation
 import me.ezrahome.libertyutils.debttracker.model.TransactionType
 import java.math.BigDecimal
-import java.time.LocalDate
 import java.util.UUID
 
 /**
@@ -14,7 +13,7 @@ data class TransactionResponseDto (
     var userId: UUID? = null,
     var contactName: String? = null,
     var transactionType: TransactionType? = null,
-    var transactionDate: LocalDate? = null,
+    var transactionDate: String? = null,
     var amount: BigDecimal? = null,
     var description: String? = null,
     var location: LibertyLocation? = null
