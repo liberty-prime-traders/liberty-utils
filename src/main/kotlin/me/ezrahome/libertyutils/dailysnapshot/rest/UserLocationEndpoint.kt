17b9480a4592901a2375b59d6b903246cb3b3866
Ezra Orina <Ezra.Orina@nisc.coop>
2025-09-02 16:13:01 -0500

    remove CrossOrigin Tags

--- src/main/kotlin/me/ezrahome/libertyutils/dailysnapshot/rest/UserLocationEndpoint.kt
+++ src/main/kotlin/me/ezrahome/libertyutils/dailysnapshot/rest/UserLocationEndpoint.kt
@@ -1,19 +1,16 @@
-package me.ezrahome.libertyutils.dailysnapshot.rest
+package me.ezrahome.libertyutils.platform.rest.sysuser
 
-import me.ezrahome.libertyutils.dailysnapshot.business.user_location.UserLocationInsertDto
-import me.ezrahome.libertyutils.dailysnapshot.business.user_location.UserLocationResponseDto
-import me.ezrahome.libertyutils.dailysnapshot.business.user_location.UserLocationService
-import me.ezrahome.libertyutils.platform.configuration.security.LibertyRoles
+import me.ezrahome.libertyutils.configuration.security.LibertyRoles
+import me.ezrahome.libertyutils.platform.business.user_location.UserLocationInsertDto
+import me.ezrahome.libertyutils.platform.business.user_location.UserLocationResponseDto
+import me.ezrahome.libertyutils.platform.business.user_location.UserLocationService
 import org.springframework.security.access.prepost.PreAuthorize
-import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
-
-@CrossOrigin
 @RestController
 @RequestMapping("secured/user-location")
 @PreAuthorize("hasRole('${LibertyRoles.ROLE_LIBERTY_ADMIN}')")
